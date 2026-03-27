9e1a110bba0d7c13a8bfea24bd20705aca5b5e0a
Nemson13A <153346816+Nemson13A@users.noreply.github.com>
2026-03-27 08:52:55 +0800

    Add files via upload
    Initial game upload

---
+++ service-worker.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = 'flip-card-memory-game-v1';
+const urlsToCache = [
+  'index.html',
+  'game.html',
+  'manifest.json',
+  'icon-192x192.png',
+  'icon-512x512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
